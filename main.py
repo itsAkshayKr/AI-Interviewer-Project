d97dcd220cc8bc87e30bb9c184200761267119f4
Akshay Kumar <121746581+itsAkshayKr@users.noreply.github.com>
2025-08-05 11:53:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from AI_interview import team_Config, interview
 import asyncio
 
-
 async def main():
-    job_position = "Business Analyst" #Enter your job profile you want to prepare for
+    job_position = input("Enter the job position you want to prepare for: ")  # User input
     team = await team_Config(job_position)
 
     async for message in interview(team):
         print('-' * 70)
         print(message)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
